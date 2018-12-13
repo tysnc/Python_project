5316471fb78ce5a62d6fb9367b8044a1a4839e78
zxa <1072310714@qq.com>
2018-12-13 21:16:05 +0800

    基本表结构和sql语句

--- common/models.py
+++ common/models.py
@@ -5,38 +5,169 @@
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-#这是一个自动生成的Django模型模块。
-#您必须手动执行以下操作来清理：
-#*重新排列模型的顺序
-#*确保每个模型都有一个包含._key=True的字段
-#*确保每个ForeignKey都已将“on_delete”设置为所需的行为。
-#*如果希望允许Django创建、修改和删除表，请删除`managed=False'行
-#可以随意重命名模型，但不要重命名db_table值或字段名。
-# on_delete=models.CASCADE
-# python manage.py shell
-# from  common.models import Dept,Emp
-# Dept.objects.all() -- Emp.objects.all()
 from django.db import models
-class Dept(models.Model):
-    no = models.IntegerField(primary_key=True,db_column='dno')
-    name = models.CharField(unique=True, max_length=10,db_column='dname')
-    location = models.CharField(max_length=20,db_column='dloc')
+
+
+class TbAgent(models.Model):
+    agentid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    tel = models.CharField(max_length=20)
+    servstar = models.IntegerField()
+    realstar = models.IntegerField()
+    profstar = models.IntegerField()
+    certificated = models.IntegerField()
 
     class Meta:
         managed = False
-        db_table = 'TbDept'
+        db_table = 'tb_agent'
 
 
-class Emp(models.Model):
-    no = models.IntegerField(primary_key=True,db_column='eno')
-    name = models.CharField(max_length=20,db_column='ename')
-    job = models.CharField(max_length=20)
-
-    mgr = models.ForeignKey('self',on_delete=models.PROTECT,db_column='mgr',blank=True,null=True)
-    sal = models.IntegerField()
-    comm = models.IntegerField(blank=True, null=True)
-    dno = models.ForeignKey('Dept', on_delete=models.PROTECT, db_column='dno', blank=True, null=True)
+class TbAgentEstate(models.Model):
+    agent_estate_id = models.AutoField(primary_key=True)
+    agentid = models.ForeignKey(TbAgent, models.DO_NOTHING, db_column='agentid')
+    estateid = models.ForeignKey('TbEstate', models.DO_NOTHING, db_column='estateid')
 
     class Meta:
         managed = False
-        db_table = 'TbEmp'
+        db_table = 'tb_agent_estate'
+        unique_together = (('agentid', 'estateid'),)
+
+
+class TbDistrict(models.Model):
+    distid = models.IntegerField(primary_key=True)
+    pid = models.ForeignKey('self', models.DO_NOTHING, db_column='pid', blank=True, null=True)
+    name = models.CharField(max_length=255)
+    intro = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_district'
+
+
+class TbEstate(models.Model):
+    estateid = models.AutoField(primary_key=True)
+    distid = models.ForeignKey(TbDistrict, models.DO_NOTHING, db_column='distid')
+    name = models.CharField(max_length=255)
+    hot = models.IntegerField(blank=True, null=True)
+    intro = models.CharField(max_length=511, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_estate'
+
+
+class TbHouseInfo(models.Model):
+    houseid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    area = models.IntegerField()
+    floor = models.IntegerField()
+    totalfloor = models.IntegerField()
+    direction = models.CharField(max_length=10)
+    price = models.IntegerField()
+    priceunit = models.CharField(max_length=10)
+    detail = models.CharField(max_length=511, blank=True, null=True)
+    mainphoto = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    street = models.CharField(max_length=255)
+    hassubway = models.IntegerField()
+    isshared = models.IntegerField()
+    hasagentfees = models.IntegerField()
+    typeid = models.ForeignKey('TbHouseType', models.DO_NOTHING, db_column='typeid')
+    userid = models.ForeignKey('TbUser', models.DO_NOTHING, db_column='userid')
+    distid = models.ForeignKey(TbDistrict, models.DO_NOTHING, db_column='distid')
+    estateid = models.ForeignKey(TbEstate, models.DO_NOTHING, db_column='estateid', blank=True, null=True)
+    agentid = models.ForeignKey(TbAgent, models.DO_NOTHING, db_column='agentid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_house_info'
+
+
+class TbHousePhoto(models.Model):
+    photoid = models.AutoField(primary_key=True)
+    houseid = models.ForeignKey(TbHouseInfo, models.DO_NOTHING, db_column='houseid')
+    path = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_house_photo'
+
+
+class TbHouseTag(models.Model):
+    house_tag_id = models.AutoField(primary_key=True)
+    houseid = models.ForeignKey(TbHouseInfo, models.DO_NOTHING, db_column='houseid')
+    tagid = models.ForeignKey('TbTag', models.DO_NOTHING, db_column='tagid')
+
+    class Meta:
+        managed = False
+        db_table = 'tb_house_tag'
+        unique_together = (('houseid', 'tagid'),)
+
+
+class TbHouseType(models.Model):
+    typeid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_house_type'
+
+
+class TbLoginLog(models.Model):
+    logid = models.BigAutoField(primary_key=True)
+    userid = models.ForeignKey('TbUser', models.DO_NOTHING, db_column='userid')
+    ipaddr = models.CharField(max_length=255)
+    logdate = models.DateTimeField()
+    devcode = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_login_log'
+
+
+class TbRecord(models.Model):
+    recordid = models.BigAutoField(primary_key=True)
+    userid = models.ForeignKey('TbUser', models.DO_NOTHING, db_column='userid')
+    houseid = models.ForeignKey(TbHouseInfo, models.DO_NOTHING, db_column='houseid')
+    recorddate = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'tb_record'
+        unique_together = (('userid', 'houseid'),)
+
+
+class TbTag(models.Model):
+    tagid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_tag'
+
+
+class TbUser(models.Model):
+    userid = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=20)
+    password = models.CharField(max_length=32)
+    realname = models.CharField(max_length=20)
+    tel = models.CharField(unique=True, max_length=20)
+    email = models.CharField(unique=True, max_length=255)
+    createdate = models.DateTimeField()
+    point = models.IntegerField()
+    isagent = models.IntegerField()
+    lastvisit = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'tb_user'
+
+
+class TbUserToken(models.Model):
+    tokenid = models.AutoField(primary_key=True)
+    token = models.CharField(max_length=32)
+    userid = models.ForeignKey(TbUser, models.DO_NOTHING, db_column='userid', unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_user_token'
